b872e2750d156e5b6fd3c31616448676cd81985a
Toni  Diep <adiep123@gmail.com>
2020-03-27 04:44:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,9 +19,9 @@ def assign_rooms(speakers)
 end
 
 def printer(prints)
-  batch_badge_creator(badges).each do |list|
+  batch_badge_creator(attendees).each do |list|
     puts list
     end
-  assign_rooms(rooms).each do |list|
+  assign_rooms(attendees).each do |list|
     puts list
   end
